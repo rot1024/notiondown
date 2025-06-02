5a97d8b255e725bf905cf78cc8964dad9a101e1b
rot1024 <aayhrot@gmail.com>
2025-06-03 02:17:49 +0900

    refactor: rename file name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { downloadImages, downloadImagesWithRetry } from "./download.ts";
 export type * from "./interfaces";
-export { Client } from "./astrotion.ts";
+export { Client } from "./client.ts";
 export { markdownToHTML } from "./md2html.ts";
