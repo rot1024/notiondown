7551f90b7ffe5de7a7e96da93e880fcaecf52a4b
rot1024 <aayhrot@gmail.com>
2025-06-04 23:53:31 +0900

    refactor: separate main

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { downloadImages, downloadImagesWithRetry } from "./download.ts";
 export { Client, type Options } from "./client.ts";
 export { Md2Html } from "./md2html.ts";
+export { main as run, type MainOptions as RunOptions } from "./main.ts";
 export type * from "./interfaces";
